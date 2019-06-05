43f7f68b94df8bdbf288afa915c011cdfa4a5b29
Niv Bekelman <nivb52@gmail.com>
2019-06-05 21:35:11 +0300

    draw by movment

--- js/canvas-main.js
+++ js/canvas-main.js
@@ -1,18 +1,38 @@
 'use strict'
 
+let gIsDraw = false
 let canvas
 let ctx
-
 let currElement = ''
 
+let currEvent
+let prevEvent
+let movementY, movementX, movement
+
+const X_FACTOR = 10
+const Y_FACTOR = 10
 
 function init() {
     canvas = document.querySelector('#my-canvas');
     ctx = canvas.getContext('2d')
-
     canvas.width = window.innerWidth - 100
     canvas.height = window.innerHeight - 200
+
+    document.documentElement.onmousemove = function (ev) {
+        currEvent = ev
+    }
+
 }
+
+setInterval(function () {
+    prevEvent;
+    if (prevEvent && currEvent) {
+        movementX = Math.abs(currEvent.screenX - prevEvent.screenX)
+        movementY = Math.abs(currEvent.screenY - prevEvent.screenY)
+        movement = Math.round(Math.sqrt(movementX ** 2 + movementY ** 2))
+    }
+    prevEvent = currEvent
+}, 100)
 
 function getColor() {
     let fillColor = document.querySelector('#fill-color').value
@@ -43,11 +63,13 @@ function draw(ev) {
         case 'line':
             drawLine(offsetX, offsetY)
             break;
+        case 'bird':
+            drawBird(offsetX, offsetY)
+            break;
     }
-    ctx.restore()
+    // ctx.restore()
 }
 
-let gIsDraw = false
 
 function onStartDraw() {
     gIsDraw = true
@@ -59,23 +81,26 @@ function onStopDraw() {
 
 function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.beginPath()
+    movementX = 0
+    movementY = 0
 }
 
 
 function drawTriangle(x, y) {
     // TODO: isFill if yes to make it fill
     // TODO: USER CHOOSE lineWidth 
+    // USE : ctx.fillStyle = getColor() //'#ff0000'
+    // AND : ctx.fill()
+
     ctx.beginPath();
     ctx.moveTo(x, y);
-    ctx.lineTo(x - 50, y - 30);
-    ctx.lineTo(x + 30, y + 50);
+    ctx.lineTo(x + 30, y - 30);
+    ctx.lineTo(movementX + X_FACTOR, movementY + Y_FACTOR);
     ctx.closePath()
     ctx.lineWidth = 1;
     ctx.strokeStyle = getColor()
-        // ctx.fillStyle = getColor() //'#ff0000'
     ctx.stroke();
-    // ctx.fill()
-
 }
 
 function drawLine(x, y) {
@@ -83,30 +108,39 @@ function drawLine(x, y) {
     ctx.lineTo(x, y)
     ctx.lineTo(x + 40, y + 40)
     ctx.closePath()
-    ctx.lineWidth = 1
+    ctx.lineWidth = 5 - movement
     ctx.strokeStyle = getColor()
     ctx.stroke();
 }
 
 function drawSquare(x, y) {
     //TODO: isFill if yes to make it fill
-    ctx.rect(x, y, 100, 100)
-    ctx.fillStyle = getColor()
-        // ctx.fillRect(x, y, 100, 100)
+    // USE : ctx.fillRect(x, y, 100, 100)
+    // AND : ctx.fill()
+
+    ctx.rect(x, y, movementX + X_FACTOR, movementY + Y_FACTOR)
+    ctx.strokeStyle = getColor()
     ctx.stroke()
-        // ctx.fill()
 }
 
 function drawArc(x, y) {
     ctx.beginPath();
-    ctx.arc(x, y, 40, 0, 1 * Math.PI);
+    ctx.arc(x, y, movement, 0, 1 * Math.PI);
+    ctx.strokeStyle = getColor()
+    ctx.stroke();
+}
+
+function drawBird(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, 25, 0, 0.25 * Math.PI);
+    ctx.arc(x - 20, y - 20, 25, 1, 0.75 * Math.PI);
     ctx.strokeStyle = getColor()
     ctx.stroke();
 }
 
 function drawCircle(x, y) {
     ctx.beginPath();
-    ctx.arc(x, y, 30, 0, 2 * Math.PI);
+    ctx.arc(x, y, movement + X_FACTOR, 0, 2 * Math.PI);
     ctx.strokeStyle = getColor()
     ctx.stroke();
 }
